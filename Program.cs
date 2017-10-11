6d71b3ebdccba7d61abcd1711d0c45a2e1ccee3a
FIC <lucas_redigolo@hotmail.com>
2017-10-11 18:31:20 -0300

    estudos de array e for

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Projeto_11_Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool UmMaiorDois = 1 > 2; // tem como padrão um valor FALSO
+            if (UmMaiorDois)
+            {
+                System.Console.WriteLine("Um é maior que dois");
+            }
+            else
+            {
+                System.Console.WriteLine("Um não é maior que dois");
+            }
+            
+            int[] idades = new int [5]; // variavel + colchetes + nome da variavel + new + tipo da variavel e [quantos retornos]
+            idades[0] = 1;
+            idades[1] = 22;
+            idades[2] = 23;
+
+            for (int teste =0; teste <idades.Length; teste++) // formula padrão para repetir todos os códigos da Array de um jeito mais prático
+            {
+                System.Console.WriteLine(idades[teste]);
+            }
+
+        }   
+    }
+}
